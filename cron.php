8ab6abae3709e02b69dc3b4d7dff57fe52ebaf97
Guido Vollbach <guido.vollbach@databay.de>
2018-05-08 11:18:03 +0200

    added cron.php

---
+++ cron.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once './Services/Cron/classes/class.ilCronStartUp.php';
+
+if($_SERVER['argc'] < 4)
+{
+	echo "Usage: cron.php username password client\n";
+	exit(1);
+}
+try {
+	$cron = new ilCronStartUp($_SERVER['argv'][3], $_SERVER['argv'][1], $_SERVER['argv'][2]);
+	$cron->initIlias();
+	$cron->authenticate();
+
+	require_once dirname(__FILE__) . '/classes/class.ilCronElectronicCourseReservePlugin.php';
+	$instance = ilCronElectronicCourseReservePlugin::getInstance();
+	$instance->getCronJobInstance(0)->run();
+}
+catch(Exception $e)
+{
+	echo $e->getMessage()."\n";
+	exit(1);
+}
+
